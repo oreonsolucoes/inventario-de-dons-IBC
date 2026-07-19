c47b5c7db6807da7374f8400a1ccbc47db874e09
oreon <107062239+oreonsolucoes@users.noreply.github.com>
2026-07-19 18:20:25 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDXNDMUUqe3XvNUPfAoh1uEYMgEKOd6Z1c",
+    authDomain: "inventario-de-dons.firebaseapp.com",
+    projectId: "inventario-de-dons",
+    storageBucket: "inventario-de-dons.firebasestorage.app",
+    messagingSenderId: "162531842551",
+    appId: "1:162531842551:web:345a6b28212025bd7736f2"
+  };
+
+// Senha simples do painel do professor (troque à vontade)
+const SENHA_PROFESSOR = "dons2026";
+
+
